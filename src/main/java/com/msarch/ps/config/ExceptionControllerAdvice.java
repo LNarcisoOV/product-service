92cf10d7b7b1d3c1ccdad5806d6fc66c697eb941
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-04-20 17:17:12 +0100

    [master] Add controller advice.

---
+++ src/main/java/com/msarch/ps/config/ExceptionControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.msarch.ps.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.persistence.NoResultException;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(NoResultException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Error noResultError(NoResultException noResultException){
+        return new Error(noResultException);
+    }
+}
