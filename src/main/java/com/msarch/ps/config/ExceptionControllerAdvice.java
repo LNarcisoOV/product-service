806641fa4fdfb4cb730df7ea0b6f42460be74531
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-04-20 17:23:13 +0100

    [master] Add advice for bad request..

--- src/main/java/com/msarch/ps/config/ExceptionControllerAdvice.java
+++ src/main/java/com/msarch/ps/config/ExceptionControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.persistence.NoResultException;
 
@@ -16,5 +17,12 @@ public class ExceptionControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Error noResultError(NoResultException noResultException){
         return new Error(noResultException);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Error methodArgumentTypeMismatchException(MethodArgumentTypeMismatchException methodArgumentTypeMismatchException){
+        return new Error(methodArgumentTypeMismatchException);
     }
 }
